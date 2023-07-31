0053ceafa11f886d3c571ad113da7b29ab915eb8
Ojja <georgesaad111@gmail.com>
2023-08-01 00:43:25 +0300

    edit in config again

--- remix.config.js
+++ remix.config.js
@@ -1,4 +1,3 @@
-const { withEsbuildOverride } = require("remix-esbuild-override");
 const { createRoutesFromFolders } = require("@remix-run/v1-route-convention");
 const {
   default: GlobalsPolyfills,
@@ -8,27 +7,7 @@ const {
 const cacheControlHeaders = {
   "Cache-Control": "public, max-age=31536000",
 };
-/**
- * Define callbacks for the arguments of withEsbuildOverride.
- * @param option - Default configuration values defined by the remix compiler
- * @param isServer - True for server compilation, false for browser compilation
- * @param isDev - True during development.
- * @return {EsbuildOption} - You must return the updated option
- */
- withEsbuildOverride((option, { isServer }) => {
-  if (isServer) {
-    option.platform = "node";
-    option.define = {
-      global: "globalThis",
-    };
-    option.plugins = [
-      GlobalsPolyfills({ buffer: true }),
-      ...option.plugins,
-    ];
-  }
 
-  return option;
-});
 
 /** @type {import('@remix-run/dev').AppConfig}*/
 module.exports = {
