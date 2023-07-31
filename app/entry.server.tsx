58dc5619fef9986801b5f096c5fc83659cca24b3
Ojja <georgesaad111@gmail.com>
2023-08-01 01:01:28 +0300

    edit in config again 5

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,53 +1,54 @@
-import { renderToString } from 'react-dom/server'
+import { renderToNodeStream } from 'react-dom/server';
 import { RemixServer } from "@remix-run/react";
-import { createInstance } from 'i18next'
-import { I18nextProvider, initReactI18next } from 'react-i18next'
-import Backend from 'i18next-fs-backend'
-import { resolve } from 'node:path'
-import i18nextOptions from './i18nextOptions'
-import i18n from './i18next.server'
+import { createInstance } from 'i18next';
+import { I18nextProvider, initReactI18next } from 'react-i18next';
+import Backend from 'i18next-fs-backend';
+import { resolve } from 'path';
+import i18nextOptions from './i18nextOptions';
+import i18n from './i18next.server';
 
-export default async function handleRequest(
-  request,
-  statusCode,
-  headers,
-  context
-) {
+addEventListener('fetch', (event) => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
   // First, we create a new instance of i18next so every request will have a
   // completely unique instance and not share any state
-  const instance = createInstance()
+  const instance = createInstance();
 
   // Then we could detect locale from the request
-  const lng = await i18n.getLocale(request)
+  const lng = await i18n.getLocale(request);
   // And here we detect what namespaces the routes about to render want to use
-  const ns = i18n.getRouteNamespaces(context)
+  const ns = i18n.getRouteNamespaces({ url: request.url });
 
   // First, we create a new instance of i18next so every request will have a
   // completely unique instance and not share any state.
   await instance
     .use(initReactI18next) // Tell our instance to use react-i18next
-    .use(Backend) // Setup our backend.init({
+    .use(Backend) // Setup our backend
     .init({
-      ...i18nextOptions, // use the same configuration as in your client side.
+      ...i18nextOptions, // use the same configuration as on the client side.
       lng, // The locale we detected above
       ns, // The namespaces the routes about to render want to use
       backend: {
         loadPath: resolve("./public/locales/{{lng}}/{{ns}}.json"),
       }
-    })
+    });
 
   // Then you can render your app wrapped in the I18nextProvider as in the
   // entry.client file
-  const markup = renderToString(
+  const markup = renderToNodeStream(
     <I18nextProvider i18n={instance}>
-      <RemixServer context={context} url={request.url} />
+      <RemixServer context={{}} url={request.url} />
     </I18nextProvider>
   );
 
-  headers.set("Content-Type", "text/html");
+  const headers = new Headers({
+    "Content-Type": "text/html; charset=UTF-8",
+  });
 
-  return new Response("<!DOCTYPE html>" + markup, {
-    status: statusCode,
-    headers: headers,
-  })
+  return new Response(markup, {
+    status: 200,
+    headers,
+  });
 }
